6ebd8a2e26e2f1315f52fa54768249d847259b15
havoc-1 <syguico@gmail.com>
2024-02-06 11:33:32 +0800

    Removed clock feature

--- server.js
+++ server.js
@@ -18,33 +18,6 @@ app.use(basicAuth({
     challenge: true
 }));
 
-let realWorldSeconds = 0;
-let inGameMinutes = 0;
-let intervalId = null;
-
-// Update real world time every second
-setInterval(() => {
-    realWorldSeconds++;
-}, 1000);
-
-// Update in-game time every 5 seconds
-setInterval(() => {
-    inGameMinutes++; // 1 in-game minute is 5 irl seconds
-}, 5000);
-
-app.get('/time', (req, res) => {
-    const realWorldDays = Math.floor(realWorldSeconds / 60 / 60 / 24);
-    const realWorldHours = Math.floor(realWorldSeconds / 60 / 60 % 24);
-    const realWorldMinutes = Math.floor(realWorldSeconds / 60 % 60);
-    const realWorldSecs = realWorldSeconds % 60;
-    const inGameDays = Math.floor(inGameMinutes / 60 / 24);
-    const inGameHours = Math.floor(inGameMinutes / 60 % 24);
-    const inGameMins = inGameMinutes % 60;
-    res.json({ 
-        realWorldTime: `${realWorldDays}:${realWorldHours}:${realWorldMinutes}:${realWorldSecs}`,
-        inGameTime: `${inGameDays}:${inGameHours}:${inGameMins}`
-    });
-});
 
 //Create route for appLogs
 app.get('/logs', async (req, res) => {
@@ -64,31 +37,17 @@ app.get('/logs', async (req, res) => {
     res.send(logs);
 });
 
-
 //Server status 
 app.get('/start', async (req, res) => {
     const container = docker.getContainer('skycoop_container');
     await container.start();
     // Start the timers when server first starts up
-    intervalId = setInterval(() => {
-        realWorldTime++;
-        inGameTime += 12; // 1 in-game minute is 5 irl seconds
-        fs.writeFileSync(path.join(__dirname, 'times.json'), JSON.stringify({ realWorldTime, inGameTime }));
-    }, 1000);
     res.send('Started');
 });
 
 app.get('/stop', async (req, res) => {
     const container = docker.getContainer('skycoop_container');
     await container.stop();
-    // Stop the timers and reset them to 0 when the server stops
-    if (intervalId) {
-        clearInterval(intervalId);
-        intervalId = null;
-    }
-    realWorldTime = 0;
-    inGameTime = 0;
-    fs.writeFileSync(path.join(__dirname, 'times.json'), JSON.stringify({ realWorldTime, inGameTime }));
     res.send('Stopped');
 });
 
@@ -98,7 +57,6 @@ app.get('/status', async (req, res) => {
     const isRunning = data.State.Running;
     res.json({ isRunning });
 });
-
 
 app.listen(4000, () => console.log('Server running on port 4000'));
 
